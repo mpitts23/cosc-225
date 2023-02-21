b1deb8ba5a319e6fd98a8d942fbbcdf390b4b23e
mpitts23 <mpitts23@amherst.edu>
2023-02-21 09:17:02 -0500

    assignment 4

--- assignment_4/cellular-automata.js
+++ assignment_4/cellular-automata.js
@@ -1,11 +1,19 @@
 function applyRule(config, rule){
 
-
+grand = document.querySelector("#header");
+header = document.createElement("h1");
+header.style.padding = "10px"
+header.style.textAlign = "center";
+header.textContent = "Rule " + rule;
+grand.appendChild(header);
 const parent = document.querySelector("#main");
 
-/* draw remaining rows */
-for(let j = 0; j <2; j++){
-    if(j = 0){
+
+
+
+// /* draw remaining rows */
+for(let j = 0; j <config.length; j++){
+    if(j == 0){
         config = config;
     }
     else{
@@ -23,6 +31,7 @@ function get_binary(n) {
         binary.push(n%2);
         n=Math.trunc(n/2);
     }
+   // binary=binary.reverse();
     return binary;
 }
 
@@ -33,13 +42,13 @@ function makeRules(config, rule){
     var right = 0;
     var config2 = new Array(config.length).fill(0);
     for(let i = 0; i < config.length; i++){
-        if(i = 0){
+        if(i == 0){
             left = config[config.length - 1];
         }
         else{
             left = config[i-1];
         }
-        if(i = config.length - 1){
+        if(i ==config.length - 1){
             right = config[0];
         }
         else{
@@ -82,20 +91,20 @@ function makeRules(config, rule){
             }
         }
     }
-    return(config2);
+    return config2;
 }
 
 function makeRow(config){
     const newRow = document.createElement("div");
-    newRow.style.height = "10px";
-    newRow.style.width = "500px";
+    newRow.style.height = 500 / config.length + "px";
+    newRow.style.width = config.length + "px";
 
     for(let i = 0; i < config.length; i++){
         const newCell = document.createElement("div");
-        newCell.style.height = "10px";
-        newCell.style.width = "10px";
+        newCell.style.height = (500/ config.length) + "px";
+        newCell.style.width = (500/ config.length) + "px";
         newCell.style.position = "absolute";
-        newCell.style.left = i*10 + "px";
+        newCell.style.left = i* (500 / config.length) + "px";
         if (config[i] == 1){
             newCell.style.backgroundColor = "rgb(0,0,0)";
         }
@@ -104,6 +113,6 @@ function makeRow(config){
         }
         newRow.appendChild(newCell);
     }
-    return(newRow);
+    return newRow;
 }
 
